589f446a600a48ba85cff24125d8aea26d2ca4d7
纪亚荣 <jiyr@wfl-ischool.cn>
2018-03-06 14:18:41 +0800

    修复bug

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,3 @@
-Rails.application.config.assets.precompile += %w( sql-formatter.min.js )
+if Rails.application.config.respond_to?(:assets)
+  Rails.application.config.assets.precompile += %w( sql-formatter.min.js )
+end
